6a7df582e4f881c5651c575792af871c8d503da9
liorsh <liorsh@shenkar.ac.il>
2019-03-05 19:50:59 +0200

    sensor

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
 });
 
 app.use("/", indexRoute);
-app.use("/start", indexRoute);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -20,21 +20,50 @@ var options = {
   encoding: "utf8"
 };
 
+var results = [];
+
 // client connection
 var client = mqtt.connect("mqtt://m24.cloudmqtt.com", options);
 
 // enable the subscription router
 var routerMqttrouter = mqttrouter.wrap(client);
 
-// subscribe to messages for 'hello/me'
-routerMqttrouter.subscribe("myTopic", function(topic, message) {
-  console.log("received", topic, message.toString());
+// subscribe to messages for client-connected
+routerMqttrouter.subscribe("client-connected", function(topic, message) {
+  console.log(
+    "received from " + topic + ": " + "client connected-> " + message.toString()
+  );
+});
+// subscribe to messages for start-timestamp
+routerMqttrouter.subscribe("start-timestamp", function(topic, message) {
+  console.log(
+    "received from " + topic + ": " + "start-timestamp-> " + message.toString()
+  );
+  results.push({ start: message.toString() });
+});
+
+// subscribe to messages for touch-time
+routerMqttrouter.subscribe("touch-time1", function(topic, message) {
+  console.log("received from " + topic + ": " + message.toString());
+  results.push({ "touch from sensor1": message.toString() });
+});
+
+// subscribe to messages for touch-time
+routerMqttrouter.subscribe("touch-time2", function(topic, message) {
+  console.log("received from " + topic + ": " + message.toString());
+  results.push({ "touch from sensor2": message.toString() });
 });
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  client.publish("myTopic", "start");
+  console.log("coach start swim");
+  client.publish("start-swim", "start");
   res.render("index", { title: "Express" });
 });
 
+router.get("/getResults", function(req, res, next) {
+  console.log("getResults");
+  res.send(results);
+  results = [];
+});
 module.exports = router;
