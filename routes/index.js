d9d727f23f7611d3c99e716b7a8611f9d88b1d93
unknown <Administrator@It70.shenkar.ac.il>
2019-03-05 16:13:52 +0200

    add mqtt router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mqtt = require("mqtt");
+var mqttrouter = require("mqtt-router");
+var options = {
+  port: 17022,
+  host: "mqtt://m24.cloudmqtt.com",
+  clientId:
+    "mqttjs_" +
+    Math.random()
+      .toString(16)
+      .substr(2, 8),
+  username: "oiidqtdo",
+  password: "0TDWlrS_abQd",
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: "MQIsdp",
+  protocolVersion: 3,
+  clean: true,
+  encoding: "utf8"
+};
+
+// client connection
+var client = mqtt.connect("mqtt://m24.cloudmqtt.com", options);
+
+// enable the subscription router
+var routerMqttrouter = mqttrouter.wrap(client);
+
+// subscribe to messages for 'hello/me'
+routerMqttrouter.subscribe("myTopic", function(topic, message) {
+  console.log("received", topic, message.toString());
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  client.publish("myTopic", "start");
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
